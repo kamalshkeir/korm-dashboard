54a57ba590afa5be43384533614b85af84e25ee6
kamalshkeir <kamalshkeir@yahoo.fr>
2023-04-23 19:45:17 +0200

    add fk

--- static/admin/all_models.js
+++ static/admin/all_models.js
@@ -11,10 +11,47 @@ let html = document.querySelector("html");
 let searchForm = document.querySelector("form.search-input");
 let page = 1;
 
+
+
+function switchTime(name) {
+  let tds = document.querySelectorAll(`td.timeFormat-${name}`);
+  if (localTime) {
+    tds.forEach(std => {
+      std.dataset.prev = std.textContent;
+      let date = new Date(std.textContent * 1000);
+      std.textContent = date.toLocaleString("fr-FR");
+    })
+  } else {
+    tds.forEach(std => {
+      if (std.dataset.prev) {
+        std.textContent = std.dataset.prev;
+      }
+    })
+  }
+}
+
+let allTimes = document.querySelectorAll(`td[class*="timeFormat-"]`);
+allTimes.forEach(t => {
+  t.dataset.prev=t.textContent;
+  let date = new Date(t.textContent * 1000);
+  t.textContent = date.toLocaleString("fr-FR");
+})
+var localTime = true;
+
+let inputsTime = document.querySelectorAll("th.timeFormat");
+inputsTime.forEach((inputt) => {
+  inputt.addEventListener("click", (e) => {
+    e.preventDefault();
+    localTime=!localTime;
+    let name = e.target.dataset.name;
+    switchTime(name);
+  })
+})
+
 let handlePostSearch = (data) => {
   if (data.rows != null) {
-    page=1;
-    document.querySelector(".tbody").innerHTML="";
+    page = 1;
+    document.querySelector(".tbody").innerHTML = "";
     if (data.rows.length > 0) {
       data.rows.forEach((row) => {
         let tr = document.createElement("tr");
@@ -36,7 +73,7 @@ let handlePostSearch = (data) => {
               `;
               break;
             default:
-              switch (typeof(row[key])) {
+              switch (typeof (row[key])) {
                 case "number":
                   if ((key !== pk) && (row[key] == 0 || row[key] == 1)) {
                     if (row[key] == 1) {
@@ -51,11 +88,19 @@ let handlePostSearch = (data) => {
                         <input id="check-${i}" name="check-${i}" type="checkbox" class="checkbox" disabled>
                         <label style="display: none;" for="check-${i}" >Checkbox</label> 
                       `;
-                    } 
+                    }
                     break;
                   } else {
+                    if (new Date(row[key]) !== undefined) {
+                      td.classList.add(`timeFormat-${key}`);
+                      if (localTime) {
+                        td.dataset.prev = row[key];
+                        let date = new Date(row[key] * 1000);
+                        row[key] = date.toLocaleString("fr-FR");
+                      }
+                    }
                     td.innerHTML = `
-                      <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                      <p>
                           ${row[key]}
                       </p>
                     `;
@@ -83,16 +128,23 @@ let handlePostSearch = (data) => {
                     if (row[key].length > 50) {
                       truncateNode(span, 50);
                     }
-                    
+                    if (row[key].includes("/") && row[key].includes(":")) {
+                      td.classList.add(`timeFormat-${key}`);
+                      if (localTime) {
+                        td.dataset.prev = row[key];
+                        let date = new Date(row[key] * 1000);
+                        row[key] = date.toLocaleString("fr-FR");
+                      }
+                    }
                     td.innerHTML = `
-                    <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                    <p>
                         ${span.textContent}
                     </p>
                   `;
-                  } else if ((row[key] == '0' || row[key] == '1'))  {
-                    let checked="";
+                  } else if ((row[key] == '0' || row[key] == '1')) {
+                    let checked = "";
                     if (row[key] == '1') {
-                      checked="checked"
+                      checked = "checked"
                     }
                     i++;
                     td.innerHTML = `
@@ -100,8 +152,19 @@ let handlePostSearch = (data) => {
                       <label style="display: none;" for="check-${i}" >Checkbox</label> 
                     `;
                   } else {
+                    if (!isNaN(row[key])) {
+                      let date = Date.parse(row[key]);
+                      if (date !== undefined) {
+                        td.classList.add(`timeFormat-${key}`);
+                        if (localTime) {
+                          td.dataset.prev = row[key];
+                          let date = new Date(row[key] * 1000);
+                          row[key] = date.toLocaleString("fr-FR");
+                        }
+                      }
+                    }
                     td.innerHTML = `
-                      <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                      <p>
                           ${row[key]}
                       </p>
                     `;
@@ -109,70 +172,70 @@ let handlePostSearch = (data) => {
                   break;
                 default:
                   td.innerHTML = `
-                    <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                    <p>
                         ${row[key]}
                     </p>
                   `;
                   break;
               }
-            
+
           }
-          tr.insertAdjacentElement("beforeend",td);
-        }) 
+          tr.insertAdjacentElement("beforeend", td);
+        })
         let td_delete = document.createElement("td");
         td_delete.innerHTML = `
             <button class="btn btn-danger deleteBtn" data-id="${row.id}">X</button>
           `;
-        tr.insertAdjacentElement("beforeend",td_delete);
+        tr.insertAdjacentElement("beforeend", td_delete);
         document.querySelector(".tbody").appendChild(tr);
         let del_btn = document.querySelector(`.deleteBtn[data-id='${row.id}']`);
-        del_btn.addEventListener("click",(e) => {
+        del_btn.addEventListener("click", (e) => {
           e.preventDefault();
           deleteFunc(del_btn);
         });
       })
-    } 
+    }
   } else {
     if (data.error) {
-      new Notification().show(data.error,"error");
+      new Notification().show(data.error, "error");
     }
   }
 }
 
-searchForm.addEventListener("submit",(e) => {
+searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let data = searchForm.search.value;
   let orderBy = searchForm.orderby.value;
-  let d= {};
+  let d = {};
   if (data !== "") {
-    d.query=data;
+    d.query = data;
   }
   if (orderBy !== "") {
-    d.orderby=orderBy;
+    d.orderby = orderBy;
   }
-  postData(`/admin/table/${modelName}/search`,d,handlePostSearch);
+  postData(`/admin/table/${modelName}/search`, d, handlePostSearch);
 })
 
 
 // initialise editor if exist on page
 document.querySelectorAll("textarea.editor").forEach((ed) => {
-  Jodit.make(ed,{
+  Jodit.make(ed, {
     enableDragAndDropFileToEditor: true,
     useSearch: false,
     uploader: {
-        "insertImageAsBase64URI": true
+      "insertImageAsBase64URI": true
     }
   });
 })
 
 createBtns.forEach(btn => {
-  btn.addEventListener("click",(e) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.toggle('active');
   })
-}) 
+})
 
-closeBtn.addEventListener("click",(e) => {
+closeBtn.addEventListener("click", (e) => {
   e.preventDefault();
   modal.classList.toggle('active');
 })
@@ -182,27 +245,27 @@ closeBtn.addEventListener("click",(e) => {
 
 /* close modal on click outside */
 
-html.addEventListener("click", function (e) {
-    if (e.target == document.body || e.target == document.querySelector(".wrapper")) {
-      if (modal.classList.contains("active")) {
-        modal.classList.remove("active");
-      }
-    } 
+document.addEventListener("click", function (e) {
+  if (e.target == document.body || e.target == document.querySelector(".wrapper")) {
+    if (modal.classList.contains("active")) {
+      modal.classList.remove("active");
+    }
+  }
 });
 
 /* CREATE ROW */
 
 let handlepostCreate = (data) => {
-      if(data.success) {
-          form.reset();
-          modal.classList.remove('active');
-          window.location.reload();
-      }else if (data.error) {
-        new Notification().show(data.error,"error");
-      }
+  if (data.success) {
+    form.reset();
+    modal.classList.remove('active');
+    window.location.reload();
+  } else if (data.error) {
+    new Notification().show(data.error, "error");
+  }
 }
 
-form.addEventListener("submit",(e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputs = document.querySelectorAll(".input");
 
@@ -210,103 +273,113 @@ form.addEventListener("submit",(e) => {
   inputs.forEach(input => {
     if (input.type == "file") {
       if (input.files) {
-        data.append(input.getAttribute("name"),input.files[0]);
+        data.append(input.getAttribute("name"), input.files[0]);
       }
-    } else if(input.type == "datetime-local") {
-      val=Date.parse(input.value).toString().substring(0,10);
+    } else if (input.type == "datetime-local") {
+      val = Date.parse(input.value).toString().substring(0, 10);
       if (isNaN(val)) {
-        val=Date.now().toString().substring(0,10);
+        val = Date.now().toString().substring(0, 10);
       }
-      data.append(input.getAttribute("name"),val);
+      data.append(input.getAttribute("name"), val);
     } else {
       let val = input.value;
       let name = input.getAttribute("name");
       if (val == "on" || val == "off") {
         if (input.checked) {
-          val=1
+          val = 1
         } else {
-          val=0
+          val = 0
         }
       }
-      data.append(name,val)
+      data.append(name, val)
     }
   });
   if (document.querySelectorAll("textarea.editor").length > 0) {
     document.querySelectorAll("textarea.editor").forEach(ed => {
       let name = ed.dataset.key;
-      data.append(name,ed.value);
+      data.append(name, ed.value);
     })
   }
-  data.append("table",modelName)
-  postFormData(`/admin/create/row`,data,handlepostCreate);
+  data.append("table", modelName)
+  postFormData(`/admin/create/row`, data, handlepostCreate);
 })
 
 
 /* IMPORT EXPORT */
 exportBtns.forEach(btn => {
-  btn.addEventListener("click",(e) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let mytype = e.target.dataset.type;
+    if (mytype == undefined) {
+      mytype=e.target.closest("svg").dataset.type;
+    }
     ask(`Do you confirm export ${modelName} ?`).then(confirmed => {
       if (confirmed) {
-        e.preventDefault();
-        window.location.href = `/admin/export/${modelName}`;
-      } 
+        if (mytype === "json") {
+          window.location.href = `/admin/export/${modelName}`;
+        } else if (mytype === "csv") {
+          window.location.href = `/admin/export/${modelName}/csv`;
+        } else {
+          console.log(mytype)
+        }
+      }
     })
   })
 })
 
 importBtns.forEach(btn => {
-  btn.addEventListener("click",(e) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
     importInput.click();
   })
 })
 
 let callbackImport = (data) => {
-  if(data) {
+  if (data) {
     if (data.success) {
-      new Notification().show(data.success,"success");
+      new Notification().show(data.success, "success");
     } else if (data.error) {
-      new Notification().show(data.error,"error");
+      new Notification().show(data.error, "error");
     }
   }
 }
-importInput.addEventListener("change",() => {
+importInput.addEventListener("change", () => {
   let data = new FormData();
   if (importInput.files) {
-    data.append("thefile",importInput.files[0]);
-    data.append("table",modelName);
+    data.append("thefile", importInput.files[0]);
+    data.append("table", modelName);
   }
   //post file to server
-  postFormData("/admin/import",data,callbackImport);
+  postFormData("/admin/import", data, callbackImport);
 })
 
 
 
 /* DELETE ROW */
 let handlepostDelete = (data) => {
-      if(data.success) {
-          new Notification().show(data.success,"success");
-          document.querySelector(`.deleteBtn[data-id='${data.id}']`).closest('tr').remove();
-      }else if (data.error) {
-          new Notification().show(data.error,"error");
-      }
+  if (data.success) {
+    new Notification().show(data.success, "success");
+    document.querySelector(`.deleteBtn[data-id='${data.id}']`).closest('tr').remove();
+  } else if (data.error) {
+    new Notification().show(data.error, "error");
+  }
 }
 
 let deleteFunc = (btn) => {
   let idd = btn.dataset.id;
   ask(`Are your sure u want to delete ?`).then(confirmed => {
     if (confirmed == true) {
-      postData(`/admin/delete/row`,{
-            "mission":"delete_row",
-            "model_name":modelName,
-            "id":idd,
-      },handlepostDelete);
-    } 
+      postData(`/admin/delete/row`, {
+        "mission": "delete_row",
+        "model_name": modelName,
+        "id": idd,
+      }, handlepostDelete);
+    }
   })
 }
 
 deletebtns.forEach((btn) => {
-  btn.addEventListener("click",(e) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
     deleteFunc(btn);
   })
@@ -328,7 +401,7 @@ let handlepostScroll = (data) => {
           switch (key) {
             case pk:
               td.innerHTML = `
-              <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+              <p>
                     <a href="/admin/get/${modelName}/${row[key]}">${row[key]}</a>
               </p>
               `;
@@ -339,9 +412,9 @@ let handlepostScroll = (data) => {
               `;
               break;
             default:
-              switch (typeof(row[key])) {
+              switch (typeof (row[key])) {
                 case "number":
-                  if ((key !== pk) && (row[key] == 0 || row[key] == 1) ) {
+                  if ((key !== pk) && (row[key] == 0 || row[key] == 1)) {
                     if (row[key] == 1) {
                       i++;
                       td.innerHTML = `
@@ -354,15 +427,23 @@ let handlepostScroll = (data) => {
                         <input id="check-${i}" name="check-${i}" type="checkbox" class="checkbox" disabled>
                         <label style="display: none;" for="check-${i}" >Checkbox</label> 
                       `;
-                    } 
+                    }
                     break;
-                  } else {
+                  } else if (!isNaN(row[key])) {
+                    if (new Date(row[key]) !== undefined) {
+                      td.classList.add(`timeFormat-${key}`);
+                      if (localTime) {
+                        td.dataset.prev = row[key];
+                        let date = new Date(row[key] * 1000);
+                        row[key] = date.toLocaleString("fr-FR");
+                      }
+                    }
                     td.innerHTML = `
-                      <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                      <p>
                         ${row[key]}
                       </p>
                     `;
-                  } 
+                  }
                   break;
                 case "boolean":
                   if (row[key]) {
@@ -386,45 +467,45 @@ let handlepostScroll = (data) => {
                     if (row[key].length > 50) {
                       truncateNode(span, 50);
                     }
-                    
+
                     td.innerHTML = `
-                    <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                    <p>
                         ${span.textContent}
                     </p>
                   `;
-                  } else if ((row[key] == '0' || row[key] == '1') || key.includes("is"))  {
-                    let checked="";
+                  } else if ((row[key] == '0' || row[key] == '1') || key.includes("is")) {
+                    let checked = "";
                     if (row[key] == '1') {
-                      checked="checked"
+                      checked = "checked"
                     }
                     i++;
                     td.innerHTML = `
                       <input id="check-${i}" name="check-${i}" type="checkbox" class="checkbox" ${checked} disabled>
                       <label style="display: none;" for="check-${i}" >Checkbox</label> 
                     `;
-                  } 
+                  }
                   break;
                 default:
                   td.innerHTML = `
-                    <p style="overflow-wrap:break-word;max-width: 20vw;margin:0 auto;">
+                    <p>
                         ${row[key]}
                     </p>
                   `;
                   break;
               }
-            
+
           }
-          tr.insertAdjacentElement("beforeend",td);
+          tr.insertAdjacentElement("beforeend", td);
         })
-        
+
         let td_delete = document.createElement("td");
         td_delete.innerHTML = `
             <button class="btn btn-danger deleteBtn" data-id="${row.id}">X</button>
           `;
-        tr.insertAdjacentElement("beforeend",td_delete);
+        tr.insertAdjacentElement("beforeend", td_delete);
         document.querySelector(".tbody").appendChild(tr);
         let del_btn = document.querySelector(`.deleteBtn[data-id='${row.id}']`);
-        del_btn.addEventListener("click",(e) => {
+        del_btn.addEventListener("click", (e) => {
           e.preventDefault();
           deleteFunc(del_btn);
         });
@@ -440,34 +521,37 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
       return;
-    } 
+    }
     page++;
     let data = {
-      "page_num":`${page}`,
-    }; 
+      "page_num": `${page}`,
+    };
     if (searchForm.orderby.value != "") {
-      data.orderby=searchForm.orderby.value;
+      data.orderby = searchForm.orderby.value;
     }
     if (searchForm.orderby.query != "") {
-      data.query=searchForm.search.value;
+      data.query = searchForm.search.value;
     }
-    
-    fetch(`/admin/table/${modelName}/search`, {  
+    let head = {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+    if (csrftoken !== undefined) {
+      head['X-CSRF-Token'] = csrftoken;
+    }
+
+    fetch(`/admin/table/${modelName}/search`, {
       method: "POST",
       body: JSON.stringify(data),
-      headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          "X-CSRF-Token":csrftoken
-      }
+      headers: head
     })
-    .then(res => res.json())
-    .then(data => {
-      handlepostScroll(data);
-    })
-    .catch(() => {
-      observer.unobserve(lastRow);
-    })
-  },{threshold:1})
+      .then(res => res.json())
+      .then(data => {
+        handlepostScroll(data);
+      })
+      .catch(() => {
+        observer.unobserve(lastRow);
+      })
+  }, { threshold: 1 })
 });
 
 observer.observe(lastRow);
