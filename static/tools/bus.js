80e1c2ba0aaba110a9f080d96d630860804dfcaa
kamalshkeir <kamalshkeir@yahoo.fr>
2024-03-13 23:33:55 +0100

    bus.js

--- static/tools/bus.js
+++ static/tools/bus.js
@@ -1,10 +1,12 @@
 class Bus {
     /**
      * Bus can be initialized without any param 'let bus = new Bus()'
-     * @param {object} options "default: {}"
+     * @param {object} options "default: {...}"
      * @param {string} options.addr "default: window.location.host"
      * @param {string} options.path "default: /ws/bus"
      * @param {boolean} options.secure "default: false"
+     * @param {boolean} options.autorestart "default: false"
+     * @param {10} options.restartevery "default: 10"
      */
     constructor(options) {
         if (options === undefined) {
@@ -13,18 +15,18 @@ class Bus {
         this.addr=options.addr || window.location.host;
         this.path=options.path || "/ws/bus";
         this.scheme=options.scheme || "ws://";
+        this.fullAddress=this.scheme+this.addr+this.path;
+        this.TopicHandlers={};
+        this.autorestart=options.autorestart || false;
+        this.restartevery=options.restartevery || 10;
         this.secure=options.secure || false;
         if (this.secure) {
             this.scheme="wss://"
         }
-        this.fullAddress=this.scheme+this.addr+this.path;
-        this.TopicHandlers={};
-        this.autorestart=false;
-        this.restartevery=10;
         this.OnOpen=() =>{};
         this.OnClose=() =>{};
         this.OnData=(data) =>{};
-        this.id=this.makeid(8);
+        this.id=this.makeid();
         this.conn=this.connect(this.fullAddress,this.callback);
     }
 
@@ -55,52 +57,13 @@ class Bus {
             if (obj.topic !== undefined) {
                 // on publish
                 if($this.TopicHandlers[obj.topic] !== undefined) {
-                    let subs;
-                    if (obj.name !== undefined) {
-                        subs = new busSubscription($this,obj.topic,obj.name);
-                    } else {
-                        subs = new busSubscription($this,obj.topic);
-                    }
+                    let subs = new busSubscription($this,obj.topic);
                     $this.TopicHandlers[obj.topic](obj,subs);          
                     return;
-                } else if(obj.name !== undefined && $this.TopicHandlers[obj.topic+":"+obj.name] !== undefined) {
-                    let subs;
-                    if (obj.name !== undefined) {
-                        subs = new busSubscription($this,"",obj.topic+":"+obj.name);
-                    } else {
-                        subs = new busSubscription($this,obj.topic);
-                    }
-                    $this.TopicHandlers[obj.topic+":"+obj.name](obj,subs);          
-                    return;
                 } else {
-                    console.log("topicHandler not found for topic:",obj.topic,obj.name,$this.TopicHandlers);
+                    console.log("topicHandler not found for topic:",obj.topic);
                 }
-            } else if (obj.name !== undefined) {
-                // on sendTo
-                let top;
-                let nam;
-                if (obj.name.includes(":")) {
-                    let sp = obj.name.split(":");
-                    top = sp[0];
-                    nam = sp.slice(1).join(":");
-                } else {
-                    if (obj.topic) {
-                        top=obj.topic
-                    }
-                    nam=obj.name
-                }
-                if($this.TopicHandlers[nam] !== undefined) {
-                    let subs= new busSubscription($this,top,nam);
-                    $this.TopicHandlers[obj.name](obj,subs)               
-                    return;
-                } else if($this.TopicHandlers[top+":"+nam] !== undefined) {
-                    let subs= new busSubscription($this,top,nam);
-                    $this.TopicHandlers[top+":"+nam](obj,subs)               
-                    return;
-                } else {
-                    console.log("topicHandler not found for name:",obj.name);
-                }
-            } 
+            }
         };
 
         $this.conn.onclose =  (e) => {
@@ -123,56 +86,35 @@ class Bus {
     }
 
     /**
-     * Subscribe subscribe to a topic with optional name
+     * Subscribe subscribe to a topic
      * @param {string} topic 
      * @param {function handler(data: string,subscription: busSubscription) {}} handler 
-     * @param {string} name 
-     * @returns 
      */
-    Subscribe(topic,handler,name="") {
-        if (name !== "") {
-            this.conn.send(JSON.stringify({
-                "action":"sub",
-                "topic":topic,
-                "name":name,
-                "id":this.id
-            }));
-            let subs = new busSubscription(this,topic,name);
-            this.TopicHandlers[topic]=handler;
-            this.TopicHandlers[topic+":"+name]=handler;
-            return subs;
-        }
+    Subscribe(topic,handler) {
         this.conn.send(JSON.stringify({
             "action":"sub",
             "topic":topic,
             "id":this.id
         }));
-        let subs = new busSubscription(this,topic,name);
+        let subs = new busSubscription(this,topic);
         this.TopicHandlers[topic]=handler;
         return subs;
     }
 
     /**
-     * Unsubscribe unsubscribe from topic, name is optional
+     * Unsubscribe unsubscribe from topic
      * @param {string} topic 
-     * @param {string} name 
      */
-    Unsubscribe(topic,name="") {
+    Unsubscribe(topic) {
         let data = {
             "action":"unsub",
             "topic":topic,
             "id":this.id
         }
-        if (name !== "") {
-            data.name=name;
-        }
         this.conn.send(JSON.stringify(data));
         if (this.TopicHandlers !== undefined) {
             delete this.TopicHandlers[topic];
         } 
-        if (name !== "" && this.TopicHandlers !== undefined) {
-            delete this.TopicHandlers[topic+":"+name];
-        }
     }
 
     /**
@@ -189,24 +131,19 @@ class Bus {
         }));
     }
 
-    /**
-     * SendToNamed send to a named topic name must be like 'client:go'
-     * @param {string} name 
+     /**
+     * PublishToID publish to client or server id
+     * @param {string} id 
      * @param {object} data 
-     * @param {string} topic 
      */
-    SendToNamed(name,data,topic="") {
-        let toSenddata = {
-            "action":"send",
-            "name":name,
+     PublishToID(id,data) {
+        this.conn.send(JSON.stringify({
+            "action":"pub_id",
+            "id":id,
             "data":data,
-            "id":this.id
-        }
-        if(topic !== "") {
-            toSenddata.topic=topic
-        }
-        this.conn.send(JSON.stringify(toSenddata)); 
-    } 
+            "from":this.id
+        }));
+    }
     
     /**
      * RemoveTopic remove a topic completely from the server bus
@@ -226,30 +163,25 @@ class Bus {
         }
     } 
 
-    makeid(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
+    makeid() {
+        return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
     }
 }
 
 /**
- * busSubscription is a class with one method allowing unsubscribing from a topic without the need of arguments like topic name
+ * busSubscription is a class with one method allowing unsubscribing from a topic
  */
 class busSubscription {
-    constructor(cl,topic,name="") {
+    constructor(cl,topic) {
         this.topic=topic;
-        this.name=name;
         this.parent=cl;
     }
     /**
      * Unsubscribe take no params, unsubscribe from the topic
      */
     Unsubscribe() {
-        this.parent.Unsubscribe(this.topic,this.name);
+        this.parent.Unsubscribe(this.topic);
     }
 }
